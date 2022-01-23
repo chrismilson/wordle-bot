6c7723c2abed943130a9b3453e0be1178fe49ee0
Chris Milson-Tokunaga <chris.w.milson@gmail.com>
2022-01-23 07:57:41 +0000

    Use json word list

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import { Args, parse } from "https://deno.land/std@0.122.0/flags/mod.ts";
-import { AutoSolver } from "./components/Solver/AutoSolver.ts";
-import { HumanPuzzle } from "./components/Puzzle/HumanPuzzle.ts";
+import { AutoSolver } from "./Solver/AutoSolver.ts";
+import { HumanPuzzle } from "./Puzzle/HumanPuzzle.ts";
+import { default as word_list } from "./word-list.json" assert { type: "json" };
 
-async function main(_args: Args): Promise<void> {
-  const word_list = await fetch(
-    "https://gist.githubusercontent.com/cfreshman/cdcdf777450c5b5301e439061d29694c/raw/de1df631b45492e0974f7affe266ec36fed736eb/wordle-allowed-guesses.txt",
-  )
-    .then(res => res.text())
-    .then(text => text.split("\n"));
-
+function main(_args: Args): void {
   // Interact with the user to get puzzle input
   new AutoSolver().solve(new HumanPuzzle(word_list, 6));
 }
 
-await main(parse(Deno.args));
+main(parse(Deno.args));
