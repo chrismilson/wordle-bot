a5a8bfed848b31a6420b2e22d1e4575bc991619f
Chris Milson-Tokunaga <13655076+chrismilson@users.noreply.github.com>
2022-02-12 09:57:40 +0000

    Fix typo

--- src/common/Result.ts
+++ src/common/Result.ts
@@ -4,18 +4,18 @@ export enum Result {
   CORRECT = "!",
 }
 
-export function result_from(n: number) -> Result {
-    let result: Result[] = [];
-    while (n > 0) {
-        let r = n % 3;
-        result.push([Result.INCORRECT, Result.PARTIAL, Result.CORRECT][r]);
-        n = (n - r) / 3;
-    }
-    return result;
+export function result_from(n: number): Result[] {
+  let result: Result[] = [];
+  while (n > 0) {
+    let r = n % 3;
+    result.push([Result.INCORRECT, Result.PARTIAL, Result.CORRECT][r]);
+    n = (n - r) / 3;
+  }
+  return result;
 }
 
 export function* all_results(length: number): Generator<Result[]> {
-    for (let i = 0; i < Math.pow(3, length); i++) {
-        yield result_from(i);
-    }
+  for (let i = 0; i < Math.pow(3, length); i++) {
+    yield result_from(i);
+  }
 }
